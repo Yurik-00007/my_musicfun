5d23ecbf4d4528a460b4c22e5e0bd5709a08686e
Timofeev Yury <yurik-007@mail.ru>
2025-10-03 12:48:31 +0300

    sp-2: use Code-Splitting

--- src/features/playlists/api/playlistsApi.ts
+++ src/features/playlists/api/playlistsApi.ts
@@ -1,27 +1,12 @@
-//https://musicfun.it-incubator.app/api/1.0/
-
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-// import type {FetchPlaylistsArgs, PlaylistsResponse} from "@/features/playlists/api/playlistsApi.types.ts";
 import type {
   CreatePlaylistArgs,
   PlaylistData,
   PlaylistsResponse,
   UpdatePlaylistArgs
 } from "@/features/playlists/api/playlistsApi.types.ts";
+import {baseApi} from "@/app/api/baseApi.ts";
 
-export const playlistsApi = createApi({
-  reducerPath: 'playlistsApi',
-  tagTypes:['Playlist'],
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_URL,
-    headers: {
-      'API-KEY': import.meta.env.VITE_API_KEY,
-    },
-    prepareHeaders: (headers) => {
-      headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
-      return headers
-    }
-  }),
+export const playlistsApi = baseApi.injectEndpoints({
   endpoints: (build) =>
     ({
       fetchPlaylists: build.query<PlaylistsResponse, void>({
